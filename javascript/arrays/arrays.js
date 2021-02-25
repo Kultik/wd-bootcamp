7f1c716175bf348731993f0a9a4b0fe45be4c0ee
Kultigin Karagenc <karagenc.kultigin@gmail.com>
2021-02-25 19:15:38 +0100

    up

---
+++ javascript/arrays/arrays.js
@@ -0,0 +1,18 @@
+let days = ['Monday', 'Tuesday', 'Osakrah'];
+
+console.log(days.length);
+
+let colors = ['Rad', 'Orange', 'Yellow'];
+
+colors[0] = 'Red';
+colors[1] = 12;
+
+colors[10] = 'Green';
+console.log(colors.length); // length = 11
+
+let beatles = ['paul', 'john', 'george'];
+beatles[3] = 'ringo';
+beatles.push('stew', 'zack');
+beatles.pop();
+
+console.log(beatles);
